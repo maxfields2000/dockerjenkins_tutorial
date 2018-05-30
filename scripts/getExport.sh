5480d559927fed0d5095df23b60ee5384e1db444
mstewart <mstewart@riotgames.com>
2018-05-29 21:19:38 -0700

    Directory rename

--- scripts/getExport.sh
+++ scripts/getExport.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker cp configIncludes.txt jenkins_master_1:/tmp/configIncludes.txt
 docker exec jenkins_master_1 bash -c "tar -czvf /tmp/jenkinsexport.tar.gz -T /tmp/configIncludes.txt"
-docker cp jenkins_master_1:/tmp/jenkinsexport.tar.gz ../jenkins2/jenkins-master/jenkinsexport.tar.gz
+docker cp jenkins_master_1:/tmp/jenkinsexport.tar.gz ../jenkins/jenkins-master/jenkinsexport.tar.gz
 
 # docker cp can't do pattern matching, so we grab everything and then delete 
 # the stuff we don't want
-rm -rf ../jenkins2/jenkins-master/jobs
-docker cp jenkins_master_1:/var/jenkins_home/jobs/ ../jenkins2/jenkins-master/
-find ../jenkins2/jenkins-master/jobs ! -type directory ! -name 'config.xml' -exec rm {} \;
+rm -rf ../jenkins/jenkins-master/jobs
+docker cp jenkins_master_1:/var/jenkins_home/jobs/ ../jenkins/jenkins-master/
+find ../jenkins/jenkins-master/jobs ! -type directory ! -name 'config.xml' -exec rm {} \;
--- scripts/getSecrets.sh
+++ scripts/getSecrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker cp secretsIncludes.txt jenkins2_master_1:/tmp/secretsIncludes.txt
 docker exec --user root jenkins2_master_1 bash -c "tar -czvf /tmp/jenkinssecrets.tar.gz -T /tmp/secretsIncludes.txt"
-docker cp jenkins2_master_1:/tmp/jenkinssecrets.tar.gz jenkinssecrets.tar.gz
+docker cp jenkins_master_1:/tmp/jenkinssecrets.tar.gz jenkinssecrets.tar.gz
